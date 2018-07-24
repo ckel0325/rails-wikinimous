6f093c4f452f242e991996def229a88984195a30
Kelvin Chow <c.kelvin.k@gmail.com>
2018-07-24 16:25:16 +0800

    onepiece wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do |var|
+Article.create(
+  title: Faker::OnePiece.character,
+  content: Faker::OnePiece.quote
+  )
+
+end
+
+puts "created 10 characters"
